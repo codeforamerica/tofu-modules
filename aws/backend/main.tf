5ad9cc9c415fd1926becac59fe1705262ebfdf87
James Armes (they/them) <jarmes@codeforamerica.org>
2024-04-05 11:52:25 -0400

    Added AWS backend module. (#1)
    Also added GitHub actions.

---
+++ aws/backend/main.tf
@@ -0,0 +1,53 @@
+locals {
+  aws_logs_path = "/AWSLogs/${data.aws_caller_identity.identity.account_id}"
+  prefix        = "${var.project}-${var.environment}"
+}
+
+data "aws_caller_identity" "identity" {}
+
+data "aws_partition" "current" {}
+
+resource "aws_kms_key" "backend" {
+  description             = "OpenTofu backend encryption key for ${var.project} ${var.environment}"
+  deletion_window_in_days = var.key_recovery_period
+  enable_key_rotation     = true
+  policy = templatefile("${path.module}/templates/key-policy.json.tftpl", {
+    account_id : data.aws_caller_identity.identity.account_id,
+    partition : data.aws_partition.current.partition,
+    bucket_arn : aws_s3_bucket.tfstate.arn
+  })
+}
+
+resource "aws_kms_alias" "backend" {
+  name          = "alias/${var.project}/${var.environment}/backend"
+  target_key_id = aws_kms_key.backend.id
+}
+
+resource "aws_dynamodb_table" "tfstate_lock" {
+  name           = "tfstate"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.backend.arn
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+}
+
+output "bucket" {
+  value = aws_s3_bucket.tfstate.id
+}
+
+output "kms_key" {
+  value = aws_kms_key.backend.id
+}
---
+++ aws/backend/s3.tf
@@ -0,0 +1,52 @@
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "${local.prefix}-tfstate"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.bucket
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  rule {
+    bucket_key_enabled = true
+
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.backend.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_logging" "tfstate" {
+  bucket        = aws_s3_bucket.tfstate.id
+  target_bucket = aws_s3_bucket.tfstate.id
+  target_prefix = "${local.aws_logs_path}/s3accesslogs/${aws_s3_bucket.tfstate.id}"
+}
+
+resource "aws_s3_bucket_policy" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+  policy = templatefile("${path.module}/templates/bucket-policy.json.tftpl", {
+    account : data.aws_caller_identity.identity.account_id
+    partition : data.aws_partition.current.partition
+    bucket : aws_s3_bucket.tfstate.bucket
+  })
+}
