df174c00a76abcc5894d428e9b1b71458862fff9
James Armes <jarmes@codeforamerica.org>
2024-07-22 19:45:38 -0400

    Expose CIDR blocks for subnets

--- aws/vpc/output.tf
+++ aws/vpc/output.tf
@@ -10,8 +10,16 @@ output "private_subnets" {
   value = module.vpc.private_subnets
 }
 
+output "private_subnets_cidr_blocks" {
+  value = module.vpc.private_subnets_cidr_blocks
+}
+
 output "public_subnets" {
   value = module.vpc.public_subnets
+}
+
+output "public_subnets_cidr_blocks" {
+  value = module.vpc.public_subnets_cidr_blocks
 }
 
 output "vpc_id" {
